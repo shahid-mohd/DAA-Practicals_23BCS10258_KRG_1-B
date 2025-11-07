f0c941cb00a28b27a5be2ab1c1dc8a1fe2924a5d
Mohd Shahid <155215858+shahid-mohd@users.noreply.github.com>
2025-11-07 11:42:31 +0530

    Add Dijkstra's algorithm implementation
    Implement Dijkstra's algorithm for shortest path calculation.

---
+++ Experiment 8/Ex - 8.cpp
@@ -0,0 +1,41 @@
+// Aim : Dijkstra Algorithm
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dijkstra(int V, vector<vector<int>> &edges, int src) {
+        vector<vector<pair<int,int>>> adj(V);
+        for (auto &e : edges) {
+            int u = e[0], v = e[1], w = e[2];
+            adj[u].push_back({v, w});
+            adj[v].push_back({u, w});
+        }
+
+        vector<int> dist(V, INT_MAX);
+        dist[src] = 0;
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({0, src});
+
+        while (!pq.empty()) {
+            int d = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if (d > dist[node]) continue;
+
+            for (auto &nbr : adj[node]) {
+                int next = nbr.first;
+                int wt = nbr.second;
+
+                if (dist[node] + wt < dist[next]) {
+                    dist[next] = dist[node] + wt;
+                    pq.push({dist[next], next});
+                }
+            }
+        }
+        return dist;
+    }
+};
